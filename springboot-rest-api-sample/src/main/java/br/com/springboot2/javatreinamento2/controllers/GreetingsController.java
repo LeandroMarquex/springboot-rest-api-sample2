5775a8868c248a5a3add6ce953562fde0864622c
java <java@DESKTOP1>
2023-01-15 11:52:08 -0300

    AULA - Criando END-POINT de listar todos usuários

--- springboot-rest-api-sample/src/main/java/br/com/springboot2/javatreinamento2/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot2/javatreinamento2/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot2.javatreinamento2.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +46,14 @@ public class GreetingsController {
 
     	usuarioRepository.save(usuario); // grava no bando de dados
         return "OLA MUNDO: " + name + "!";
+    }
+    @GetMapping(value = "listatodos") // primeito metodo de api
+    @ResponseBody // Retorna os dados para o corpo da resposta
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll(); // executa a consulta no bando de dados
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+    	
     }
 }
